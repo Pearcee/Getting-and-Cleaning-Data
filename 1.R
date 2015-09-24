1529519654074e60240fce64475a866355072247
Pearcee <Sjpearce@gmail.com>
2015-09-24 23:00:24 +0100

    23
    2

--- 1.R
+++ 1.R
@@ -96,6 +96,7 @@ library(plyr);
 Data2<-aggregate(. ~subject + activity, Data, mean)
 Data2<-Data2[order(Data2$subject,Data2$activity),]
 write.table(Data2, file = "tidydata.txt",row.name=FALSE)
+write.csv(Data2, file = "tidydata.csv")
 
 # Prouduce Codebook
 
